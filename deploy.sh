30cabe3a2999570cdd603a21e76043872625626d
Chris Rimmer <chris@crimmer.co.uk>
2017-09-09 22:43:06 +0100

    fix deploy directories and permissions

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://${IP_ADDRESS}/eu4calculator.com/ \;
+find dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://${IP_ADDRESS}/eu4calculator.com/{} -Q '-SITE CHMOD 644 /eu4calculator.com/{}' \;

446bad3bbe594a2229fb301fcc78d682606b77c1
Chris Rimmer <chris@crimmer.co.uk>
2017-09-09 21:42:17 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-ls -l
-echo $FTP_USER
-curl --ftp-create-dirs -T ./dist/* -u $FTP_USER:$FTP_PASSWORD ftp://${IP_ADDRESS}/eu4calculator.com/
+find dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://${IP_ADDRESS}/eu4calculator.com/ \;

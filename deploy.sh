380bdfb1f9ec7df2bc96a710096dc30c0a3fec8e
Chris Rimmer <chris@crimmer.co.uk>
2017-09-10 15:14:52 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-find dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://${IP_ADDRESS}/eu4calculator.com/{} -Q '-SITE CHMOD 644 /eu4calculator.com/{}' \;
+cd dist
+find ./ -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://${IP_ADDRESS}/eu4calculator.com/{} -Q '-SITE CHMOD 644 /eu4calculator.com/{}' \;

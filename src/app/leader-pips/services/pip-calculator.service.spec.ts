8c200aea819767843191d520f21e49b2e6d1382f
Chris Rimmer <chris@crimmer.co.uk>
2017-10-29 20:11:14 +0000

    calculate min pips test

--- src/app/leader-pips/services/pip-calculator.service.spec.ts
+++ src/app/leader-pips/services/pip-calculator.service.spec.ts
@@ -31,4 +31,12 @@ describe('PipCalculatorService', () => {
     expect(service.modifyTradition(LeaderType.Ruler, 100, 3)).toEqual(71);
     expect(service.modifyTradition(LeaderType.Heir, 100, 3)).toEqual(71);
   }));
+
+  it('should calculate minimum pips', inject([PipCalculatorService], (service: PipCalculatorService) => {
+    expect(service.calculateMinPips(LeaderType.General, 0, 0, 0)).toEqual(1);
+    expect(service.calculateMinPips(LeaderType.General, 100, 0, 0)).toEqual(7);
+    expect(service.calculateMinPips(LeaderType.General, 100, 0, 1)).toEqual(8);
+    expect(service.calculateMinPips(LeaderType.General, 100, 0, 100)).toEqual(24);
+    expect(service.calculateMinPips(LeaderType.Admiral, 100, 0, 100)).toEqual(18);
+  }));
 });

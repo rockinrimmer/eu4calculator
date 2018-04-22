86f5a6726c39555ae2a390d9bd5fe84c14bc366b
Chris Rimmer <chris@crimmer.co.uk>
2018-04-22 21:07:08 +0100

    try to fix running tests

--- karma.conf.js
+++ karma.conf.js
@@ -28,13 +28,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     singleRun: false,
     customLaunchers: {
-        Chrome_travis_ci: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
-        }
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
     }
   };
 

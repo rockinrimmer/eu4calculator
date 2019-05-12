3d43cc48d24c2739bb516325a76fe5ec256e80e3
Chris Rimmer <chris@crimmer.co.uk>
2019-05-12 23:32:59 +0100

    update to angular 7 (#109)
    * update to angular 7
    
    * more dependency updates and add circleci
    
    * remove command
    
    * change to just install
    
    * use npm run
    
    * add quotes
    
    * use package.json scripts
    
    * final using circleci, remove travisci

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd dist
-find ./ -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://${IP_ADDRESS}/eu4calculator.com/{} -Q '-SITE CHMOD 644 /eu4calculator.com/{}' \;
